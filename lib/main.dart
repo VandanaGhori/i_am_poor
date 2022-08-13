afad8e5f9eed2d00fb460c8ea97ea32d4743b845
Vandana Ghori <vandanaghori19@gmail.com>
2022-08-12 22:22:10 -0400

    I am poor app built with two routes...

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,85 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MaterialApp(
-    home: Scaffold(
-      backgroundColor: Colors.grey,
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Builder(builder: (BuildContext context) {
+        return const FirstRoute();
+      }),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+class FirstRoute extends StatelessWidget {
+  const FirstRoute({Key? key}) : super(key: key);
+
+  // This widget is a root widget of our application.
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
       appBar: AppBar(
-        title: const Text('I am Poor'),
+        backgroundColor: Colors.grey[800],
+        title: const Text('I am poor'),
       ),
-    ),
-    debugShowCheckedModeBanner: false,
-  ));
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Expanded(child: Image.asset("images/poor_boy.png")),
+            ElevatedButton(
+              style: ButtonStyle(
+                backgroundColor: MaterialStateProperty.all(Colors.grey[800]),
+              ),
+              child: const Text('Go to next'),
+              onPressed: () {
+                // Navigate to second route when tapped.
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => const SecondRoute()));
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class SecondRoute extends StatelessWidget {
+  const SecondRoute({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.grey[800],
+        title: const Text('I am poor'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text('Thank you for visiting me!'),
+            const Text(''),
+            ElevatedButton(
+              style: ButtonStyle(
+                backgroundColor: MaterialStateProperty.all(Colors.grey[800]),
+              ),
+              child: const Text('Go Back'),
+              onPressed: () {
+                Navigator.pop(context);
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
 }
